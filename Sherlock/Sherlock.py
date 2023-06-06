eeaf0ec382956e68c932a054100a4e8b17920d33
Ayyappan <61587800+Ayyappan1999@users.noreply.github.com>
2023-06-06 12:25:47 +0530

    Create Sherlock.py

---
+++ Sherlock/Sherlock.py
@@ -0,0 +1,50 @@
+Certainly! Here's a cheatsheet for the Sherlock tool, a popular OSINT (Open Source Intelligence) tool used for username and email address enumeration across various online platforms:
+
+```
++-------------------------------------------------------+
+|                    Sherlock Tool                       |
++-------------------------+-----------------------------+
+|        Command          |         Description         |
++-------------------------+-----------------------------+
+| python3 sherlock.py     | Run Sherlock tool           |
+| <username/email>        |                             |
++-------------------------+-----------------------------+
+| python3 sherlock.py     | Run Sherlock tool with      |
+| --multiple              | multiple usernames/emails   |
+| <file_path>             | from a file                 |
++-------------------------+-----------------------------+
+| python3 sherlock.py     | Run Sherlock tool with a    |
+| --url                   | target URL to check for     |
+| <url>                   | username/email presence     |
++-------------------------+-----------------------------+
+| python3 sherlock.py     | Run Sherlock tool with a    |
+| --proxies               | proxy configuration         |
+| <proxy>                 |                             |
++-------------------------+-----------------------------+
+| python3 sherlock.py     | Run Sherlock tool with a    |
+| --timeout               | timeout value for each      |
+| <timeout>               | request (in seconds)        |
++-------------------------+-----------------------------+
+| python3 sherlock.py     | Run Sherlock tool with a    |
+| --color                 | enable colored output       |
++-------------------------+-----------------------------+
+| python3 sherlock.py     | Run Sherlock tool with      |
+| --no-color              | disable colored output      |
++-------------------------+-----------------------------+
+| python3 sherlock.py     | Run Sherlock tool with a    |
+| --output                | specify an output file      |
+| <output_file>           | to save results             |
++-------------------------+-----------------------------+
+| python3 sherlock.py     | Run Sherlock tool with      |
+| --verbose               | enable verbose mode         |
++-------------------------+-----------------------------+
+| python3 sherlock.py     | Run Sherlock tool with      |
+| --quiet                 | enable quiet mode           |
++-------------------------+-----------------------------+
+| python3 sherlock.py     | Run Sherlock tool with a    |
+| --version               | display the version         |
++-------------------------+-----------------------------+
+| python3 sherlock.py     | Run Sherlock tool with      |
+| --help                  | display help menu           |
++-------------------------+-----------------------------+
+```
